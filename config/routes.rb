1e948c6b61106383e9845c36046de618800015d0
Vladyslav <vladyslav.teleleiko@evowill.com>
2021-12-06 19:46:30 +0200

    Day 3

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
+  # Adding routes for users
+  get 'users/profile', to: 'users#profile'
+
+  resources :categories
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20211205174447_add_name_to_users.rb
+++ db/migrate/20211205174447_add_name_to_users.rb
@@ -2,7 +2,7 @@
 
 class AddNameToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :name, :string
-    add_column :users, :surname, :string
+    add_column :users, :name, :text
+    add_column :users, :surname, :text
   end
 end
